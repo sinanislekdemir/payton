54a3755da0b7f7849ad978c8dc75769b74814523
sinan islekdemir <sinan@islekdemir.com>
2019-07-08 18:57:17 +0300

    Pypi version 0.0.2.15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Payton",
-    version="0.0.2.14",
+    version="0.0.2.15",
     author="Sinan ISLEKDEMIR",
     author_email="sinan@islekdemir.com",
     # Packages
