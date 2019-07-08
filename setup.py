478e9049a77410e01028d230cd406840c424130b
sinan islekdemir <sinan@islekdemir.com>
2019-07-08 20:36:31 +0300

    0.0.2.16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Payton",
-    version="0.0.2.15",
+    version="0.0.2.16",
     author="Sinan ISLEKDEMIR",
     author_email="sinan@islekdemir.com",
     # Packages
