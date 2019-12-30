cdb391ffba6f0073ca4222b9f53544e9ad805eef
sinan islekdemir <sinan@islekdemir.com>
2019-12-30 23:43:54 +0200

    0.0.2.54

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
 
 setup(
     name="Payton",
-    version="0.0.2.53",
+    version="0.0.2.54",
     author="Sinan ISLEKDEMIR",
     author_email="sinan@islekdemir.com",
     # Packages
