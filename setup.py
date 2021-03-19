bf7fa98602af47bda8e775d78e2898ad7b831fed
Sinan Islekdemir <sinan@islekdemir.com>
2021-03-19 09:05:32 +0200

    Release new version because of Pillow upgrade

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
 
 setup(
     name="Payton",
-    version="v0.1.3",
+    version="v0.1.4",
     author="Sinan ISLEKDEMIR",
     author_email="sinan@islekdemir.com",
     # Packages
