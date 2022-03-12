2c5ae72e928ffed3cdc22827aed82d90d5b1a1b9
Sinan Islekdemir <sinan@islekdemir.com>
2022-03-12 23:10:20 +0200

    Ordering of install requires

--- setup.py
+++ setup.py
@@ -26,11 +26,11 @@ setup(
     python_requires=">3.8",
     # Dependent packages (distributions)
     install_requires=[
-        "Cython>=0.29.28",
-        "numpy>=1.22.3",
         "Pillow>=9.0.1",
         "PyOpenGL>=3.1.6",
         "pyrr>=0.10.3",
         "PySDL2>=0.9.11",
+        "numpy>=1.22.3",
+        "Cython>=0.29.28",
     ],
 )
