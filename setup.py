3658388cb47adb9a6f079574224eb37a5d4225d2
Sinan Islekdemir <sinan@islekdemir.com>
2022-03-29 20:16:15 +0300

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 8):
 
 setup(
     name="Payton",
-    version="v1.0.3",
+    version="v1.1.0",
     author="Sinan ISLEKDEMIR",
     author_email="sinan@islekdemir.com",
     # Packages
